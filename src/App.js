1262374cf0a10798590713290bc6db1913d50543
Khalid Abbas <khalid@uforiainfortech.com>
2022-09-13 11:17:30 -0700

    added comps

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageDesigner from "./components/designer/designer";
+
+function App() {
+  return (
+    <div className="App">
+      <ImageDesigner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/fabricOverrids/index.js
@@ -0,0 +1,9 @@
+import {fabric} from 'fabric';
+
+fabric.Object.prototype.set({
+    borderColor:'#33333',
+    cornerStyle:"circle",
+    cornerColor:"#33333",
+    cornerSize : 10,
+
+})
